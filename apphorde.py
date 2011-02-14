2811bbeed67419ecd58a211c801552eeb7040ebf
Craig Campbell <craigsc333@gmail.com>
2011-02-14 15:48:17 -0500

    Lots of new stuff.

--- apphorde.py
+++ apphorde.py
@@ -6,6 +6,10 @@ import tornado.web
 from tornado.options import define, options
 import tornado.options
 import os.path
+from pymongo import Connection
+from pymongo.objectid import ObjectId
+import re
+import bcrypt
 
 define("port", default=8888, type=int, help="port to listen on")
 
@@ -13,22 +17,89 @@ class Application(tornado.web.Application):
 	def __init__(self):
 		handlers = [
 			(r"/", HomeHandler),
-			(r"/newest", HomeHandler),
-			(r"/how", HowHandler),
-			(r"/signin", SignInHandler),
-			(r"/start", SignUpHandler),
+			(r"/register", RegisterHandler),
+			(r"/login", LoginHandler)
 		]
 		settings = {
 			"static_path": os.path.join(os.path.dirname(__file__), "static"),
 			"template_path": os.path.join(os.path.dirname(__file__), "templates"),
-			"ui_modules": uimodules,
+			#"ui_modules": uimodules,
+			#"xsrf_cookies": True,
+			#"cookie_secret":
+			"login_url": "/login",
 			"debug": True,
 		}
 		tornado.web.Application.__init__(self, handlers, **settings)
 		
-class HomeHandler(RequestHandler):
+		self.db = Connection().apphorde
+		
+class BaseHandler(tornado.web.RequestHandler):
+	@property
+	def db(self):
+		return self.application.db
+		
+	def get_current_user(self):
+		user_id = self.get_secure_cookie("")
+		if not user_id: return None
+		return self.db.users.find_one({'_id': ObjectId(user_id)})
+
+class HomeHandler(BaseHandler):
 	def get(self):
-		self.render("index.html")	
+		self.render("index.html")
+		
+class RegisterHandler(BaseHandler):
+	def get(self):
+		self.render("register.html")
+		
+	def post(self):
+		#validate registration info
+		email = self.get_argument("email", None)
+		password = self.get_argument("password", None)
+		confirm_password = self.get_argument("confirm_password", None)
+		accept_tos = self.get_argument("accept_tos", None)
+		errors = []
+		if email:
+			if not re.match(r"^[^\s@]+@[^\s@]+\.[^\s@]{2,}$", email):
+				errors.append("Email must be of the format 'something@something.xxx'.")
+			else:
+				if self.db.users.find({'email': email}).count() != 0:
+					errors.append("The supplied email has already been registered. <a href='/recover'>Forgot your password?</a>")
+		else: errors.append("Email is required.")
+		
+		if password:
+			if len(password) < 6 or len(password) > 32:
+				errors.append("Password must be 6-32 characters in length.")
+			else: 
+				if confirm_password and password != confirm_password:
+					errors.append("Password and password confirmation must match.")
+		else: errors.append("Password is required")
+		
+		if not confirm_password: errors.append("Password confirmation is required.")
+		
+		if not accept_tos or accept_tos != "on":
+			errors.append("You must agree to the Terms of Service to register.")
+		
+		if len(errors) == 0:
+			#insert new user entry
+			user_id = self.db.users.insert({'email': email, 'password': bcrypt.hashpw(password, bcrypt.gensalt())})
+			self.redirect("/dashboard")
+		else:
+			self.render("register.html", errors=errors)
+	
+class LoginHandler(BaseHandler):
+	def get(self):
+		self.render("login.html")
+		
+	def post(self):
+		email = self.get_argument("email", None)
+		password = self.get_argument("password", None)
+		user = self.db.users.find_one({'email': email}, {'password': 1})
+		if user and bcrypt.hashpw(password, user['password']) == user['password']:
+			print 'logged in!'
+		else:
+			print 'failure'
+		self.redirect("/login")
+		
 
 if __name__ == "__main__":
 	tornado.options.parse_command_line()
